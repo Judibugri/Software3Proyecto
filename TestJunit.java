61da31cc6990e95ac3597ade5703bb33589d92b0
Juanda <jdvasquezr@uqvirtual.edu.co>
2016-09-07 16:26:03 -0500

    Documentar la clase TestJunit
    Agregar una descripción de la funcionalidad de la clase y los autores

--- TestJunit.java
+++ TestJunit.java
@@ -1,8 +1,14 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Clase que se encarga de realizar las pruebas
+ * @author Juan Esteban Quintero Bolaños
+ * @author Juan Diego Buitrago Grisales
+ * @author Juan David Vasquez Rojas
+ */
 public class TestJunit {
-	
+
    String message = "Hello World";	
    MessageUtil messageUtil = new MessageUtil(message);
 
