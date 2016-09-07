3f6d182f67050d8b7e6200112d9c44937acd7eb1
Judibugri <judibugri@gmail.com>
2016-09-07 17:26:38 -0500

    Update HelloWorld.java
    Documentación clase principal

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,9 @@
 public class HelloWorld {
+   /**
+   *
+   *Método principal de ejecución
+   * 
+   **/
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
